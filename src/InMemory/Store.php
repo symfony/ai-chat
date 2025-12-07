70e0a9d2ec139145983b75508b3934391887ec5a
Oskar Stark <oskarstark@googlemail.com>
2025-12-07 23:20:48 +0100

    [Chat] Move InMemory store from Bridge to Chat root

---
+++ src/InMemory/Store.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Chat\InMemory;
+
+use Symfony\AI\Chat\ManagedStoreInterface;
+use Symfony\AI\Chat\MessageStoreInterface;
+use Symfony\AI\Platform\Message\MessageBag;
+
+/**
+ * @author Christopher Hertel <mail@christopher-hertel.de>
+ */
+final class Store implements ManagedStoreInterface, MessageStoreInterface
+{
+    /**
+     * @var MessageBag[]
+     */
+    private array $messages = [];
+
+    public function __construct(
+        private readonly string $identifier = '_message_store_memory',
+    ) {
+    }
+
+    public function setup(array $options = []): void
+    {
+        $this->messages[$this->identifier] = new MessageBag();
+    }
+
+    public function save(MessageBag $messages): void
+    {
+        $this->messages[$this->identifier] = $messages;
+    }
+
+    public function load(): MessageBag
+    {
+        return $this->messages[$this->identifier] ?? new MessageBag();
+    }
+
+    public function drop(): void
+    {
+        $this->messages[$this->identifier] = new MessageBag();
+    }
+}
